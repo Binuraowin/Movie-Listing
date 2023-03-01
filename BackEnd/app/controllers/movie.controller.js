4781db08e7d94efe949f9f8d434f1453e2649a60
binura-owin <binura.owin1@gmail.com>
2023-03-02 02:30:21 +0530

    added get genres function

--- BackEnd/app/controllers/movie.controller.js
+++ BackEnd/app/controllers/movie.controller.js
@@ -1,8 +1,14 @@
 const asyncHandler = require("../middleware/async");
 const { makeResponse } = require("../helpers/make_response");
-const {getMovieList} = require("../services/movie.service")
+const {getMovieList, getMovieGenresList} = require("../services/movie.service")
 
 exports.get_movie_list = asyncHandler(async (req, res) => {
+  console.log("Movie")
     const moviesRes = await getMovieList()
     return makeResponse(res, 200, true, moviesRes, "Movie");
+  });
+
+  exports.get_genres_list = asyncHandler(async (req, res) => {
+    const genresRes = await getMovieGenresList()
+    return makeResponse(res, 200, true, genresRes, "Genres");
   });
--- BackEnd/app/routes/movie.route.js
+++ BackEnd/app/routes/movie.route.js
@@ -4,5 +4,6 @@ const MovieController = require("../controllers/movie.controller");
 const MovieRouter = express.Router();
 
 MovieRouter.get("/", MovieController.get_movie_list);
+MovieRouter.get("/get_genres", MovieController.get_genres_list);
 
 module.exports = MovieRouter;
