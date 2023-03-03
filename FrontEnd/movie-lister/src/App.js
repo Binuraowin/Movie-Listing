d523642da59b0fc3b615e3ed29c71671a4f5d823
binura-owin <binura.owin1@gmail.com>
2023-03-03 20:49:10 +0530

    added login Ui

--- FrontEnd/movie-lister/src/App.js
+++ FrontEnd/movie-lister/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import Login from './screens/Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Movie List Login</h1>
+    <div className="container m-40 mx-auto">
+      <Login/>
+      {/* <h1 className="text-2xl font-bold text-cyan-800">
+        Tailwind 3 With React Js
+      </h1>
+      <button className="px-6 py-2 text-sm text-white rounded shadow-xl bg-emerald-500">
+        success
+      </button> */}
     </div>
   );
 }
 
 export default App;
-
-
